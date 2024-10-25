410a83e10bf203b46a874339db60a69b24fa1163
SadaLearningHub <sadalearninghub@gmail.com>
2024-10-25 07:05:49 +0530

    Update SpringBootCloudGatewayApplication.java

--- src/main/java/com/example/demo/SpringBootCloudGatewayApplication.java
+++ src/main/java/com/example/demo/SpringBootCloudGatewayApplication.java
@@ -9,5 +9,24 @@ public class SpringBootCloudGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCloudGatewayApplication.class, args);
 	}
+	//	@Bean
+//	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//		return builder.routes()
+//				.route(r -> r.path("/posts/**")
+//						.filters(f -> f
+//							.prefixPath("/api")
+//							.addResponseHeader("X-Powered-By","DanSON Gateway Service")
+//						)
+//						.uri("http://localhost:8081")
+//				)
+//				.route(r -> r.path("/comments/**")
+//						.filters(f -> f
+//								.prefixPath("/api")
+//								.addResponseHeader("X-Powered-By","DanSON Gateway Service")
+//						)
+//						.uri("http://localhost:8082")
+//				)
+//				.build();
+//	}
 
 }
